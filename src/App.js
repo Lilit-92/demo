ff7ccade232bc60ae117cf460496cef57d135561
Lilit-92 <hovhannisyanlilit92@gmail.com>
2020-11-14 20:54:09 +0400

    firstHomework

--- src/App.js
+++ src/App.js
@@ -15,35 +15,36 @@ class Print extends Component{
 
    render(){
      return(
+       <>
+        <div>   
+           {/* <h1 className="head">Animals</h1> */}
+        </div>
         <div className="parent">
           <div>
-              <b>Description</b>
+              <b className="des">Description</b>
               <p>
-                name: {this.props.name}
+                <b>name:</b> {this.props.name}
               </p>
               <p>
-                color: {this.props.color}
+                <b>color:</b> {this.props.color}
               </p>
               <p>
-                type: {this.props.type}
+                <b>type:</b> {this.props.type}
               </p>
               <p>
-                home:{this.props.home}
+                <b>home:</b> {this.props.home}
               </p>
               <p> 
                 <a href={this.props.more}>more about {this.props.name}</a>
               </p>
           </div>
           <div>
-              <b> 
-                {this.props.name}
-              </b>
               <p>
                 <img src={this.props.photo} />
               </p>
-
           </div>
         </div>
+        </>
      )
    }
 }
@@ -56,8 +57,8 @@ function App() {
         <Print name="Fox" color="orange" type="wild" photo={fox} home="forest" more="https://en.wikipedia.org/wiki/Fox"/>
         <Print name=" Wolf" color="grey" type="wild" photo={wolf} home="forest" more="https://en.wikipedia.org/wiki/Wolf"/>
         <Print name=" Bear" color="grey, white" type="wild" photo={bear} home="forest" more="https://en.wikipedia.org/wiki/bear"/>
-        <Print name="Horse" color="grey" type="wild" photo={horse} home="forest" more="https://en.wikipedia.org/wiki/horse"/>
-        <Print name="Rabbit" color="grey" type="wild" photo={rabbit} home="forest" more="https://en.wikipedia.org/wiki/rabbit"/>
+        <Print name="Horse" color="black, white, braun" type="wild" photo={horse} home="forest" more="https://en.wikipedia.org/wiki/horse"/>
+        <Print name="Rabbit" color="grey, white" type="wild" photo={rabbit} home="forest" more="https://en.wikipedia.org/wiki/rabbit"/>
     </div>
   );
 }
