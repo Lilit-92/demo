c6ab1203b52596de738825061485da15e859204d
Lilit-92 <hovhannisyanlilit92@gmail.com>
2020-11-16 15:25:17 +0400

    homeworkOne

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import "./App.css";
+import React from 'react';
+import Print from './classes';
 import fox from './fox1.jpg';
 import wolf from './wolf.jpg';
 import bear from './bear.jpg';
@@ -7,53 +7,12 @@ import horse from './hors.jpg';
 import rabbit from './rabbit.jpg';
 
 
-
-class Print extends Component{
-   constructor(props){
-     super(props);
-   };
-
-   render(){
-     return(
-       <>
-        <div>   
-           {/* <h1 className="head">Animals</h1> */}
-        </div>
-        <div className="parent">
-          <div>
-              <b className="des">Description</b>
-              <p>
-                <b>name:</b> {this.props.name}
-              </p>
-              <p>
-                <b>color:</b> {this.props.color}
-              </p>
-              <p>
-                <b>type:</b> {this.props.type}
-              </p>
-              <p>
-                <b>home:</b> {this.props.home}
-              </p>
-              <p> 
-                <a href={this.props.more}>more about {this.props.name}</a>
-              </p>
-          </div>
-          <div>
-              <p>
-                <img src={this.props.photo} />
-              </p>
-          </div>
-        </div>
-        </>
-     )
-   }
-}
-
-
-
 function App() {
   return (
     <div>
+       <div>   
+           <h1 className="head">Animals</h1>
+        </div>
         <Print name="Fox" color="orange" type="wild" photo={fox} home="forest" more="https://en.wikipedia.org/wiki/Fox"/>
         <Print name=" Wolf" color="grey" type="wild" photo={wolf} home="forest" more="https://en.wikipedia.org/wiki/Wolf"/>
         <Print name=" Bear" color="grey, white" type="wild" photo={bear} home="forest" more="https://en.wikipedia.org/wiki/bear"/>
---
+++ src/classes.jsx
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import "./App.css";
+
+
+
+class Print extends Component{
+    constructor(props){
+      super(props);
+    };
+ 
+    render(){
+      return(
+     
+        <div className="parent">
+          <div>
+              <b className="des">Description</b>
+              <p>
+                <b>name:</b> {this.props.name}
+              </p>
+              <p>
+                <b>color:</b> {this.props.color}
+              </p>
+              <p>
+                <b>type:</b> {this.props.type}
+              </p>
+              <p>
+                <b>home:</b> {this.props.home}
+              </p>
+              <p> 
+                <a href={this.props.more}>more about {this.props.name}</a>
+              </p>
+          </div>
+          <div>
+              <p>
+                <img src={this.props.photo} />
+              </p>
+          </div>
+        </div>
+
+     )
+   }
+}
+
+export default Print;
