a3947632c041b6b5b139d3c938928fe34676a7b7
Lilit-92 <hovhannisyanlilit92@gmail.com>
2020-11-24 19:30:28 +0400

    homeworkThree

--- src/Lesson2/Counter.jsx
+++ src/Lesson2/Counter.jsx
@@ -6,45 +6,65 @@ class Counter extends Component{
     constructor(){
         super();
         this.Decrease=this.Decrease.bind(this);
-        this.Increase=this.Increase.bind(this);
     }
 
     state={
-        num: 5,
+        num: 1,
     }
 
-    Increase(){
-        this.setState({
-            num:this.state.num+1
-        })
-    };
-
-    Decrease(){
-        if (this.state.num<=0) {
-            this.setState({   
-                num:this.state.num
-            })
-        }else{
+    Decrease(type){
+        if (type==="decrease") {
+           
+            if (this.state.num===1||this.state.num===0) { 
+                this.setState({       
+                    num:this.state.num-1
+                })
+                
+                document.getElementsByClassName("dec")[0].setAttribute("disabled", "disabled");     
+            }
+            else{
+            
             this.setState({       
                 num:this.state.num-1
             })
+        }
+           
+        }
+        else if(type==="increase"){               
+                if (this.state.num===0) {
+                    document.getElementsByClassName("disabled")[0].removeAttribute("disabled")
+                }
+            this.setState({       
+                num:this.state.num+1
+            })
+        }
+        else{
+            this.setState({       
+                num:0,
+            })
+                document.getElementsByClassName("dec")[0].setAttribute("disabled", "disabled");
         }
     };
 
 
     render(){
         return(
+            <>
             <div className="parent">
                 <div>
-                    <button onClick={this.Decrease} className="dec">-</button>
+                    <button onClick={()=>this.Decrease("decrease")} className={this.state.num<=0?"disabled":"dec"}>-</button>
                 </div>
                 <div>
                     <p className="num">{this.state.num}</p>
                 </div>
                 <div>
-                    <button onClick={this.Increase} className="inc">+</button>
+                    <button onClick={()=>this.Decrease("increase")} className="inc">+</button>
                 </div>
+                <div className="reset"><button onClick={()=>this.Decrease("reset")}>Reset</button></div>
+                
             </div>
+           
+            </>
         )
     }
 }
